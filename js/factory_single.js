5c1a259386a2896508afaf5cef3905fdc90352c5
Matei Gheorghiu <mig5@njit.edu>
2013-02-24 20:19:15 -0500

    Converted to use DOM

--- js/factory_single.js
+++ js/factory_single.js
@@ -1,44 +1,42 @@
 //FACTORY SETUP
 
-function Car(options) { //FACTORY OBJECT METHOD TO CREATE A CAR OBJECT
-	this.doors = options.doors || 4;
-	this.state = options.state || "brand new";
-	this.color = options.color || "silver";
+function Create_a(options) { //FACTORY OBJECT METHOD TO CREATE A CAR OBJECT
+	var link = document.createElement('a');
+	link.href = options.href;
+	link.id = options.id;
+	link.className = options.className;
+	link.textContent = options.textContent;
 }
 
 
-function Truck(options) { //FACTORY OBJECT METHOD TO CREATE A TRUCK OBJECT
-	this.state = options.state || "used";
-	this.wheelSize = options.wheelSize || "large";
-	this.color = options.color || "blue";
+function Create_img(options) { //FACTORY OBJECT METHOD TO CREATE A TRUCK OBJECT
+	var pic = document.createElement('img');
+	pic.src = options.src;
+	pic.id = options.id;
+	pic.className = options.className;
+	pic.textContent = options.textContent;
 }
 
-function Boat(options) { //FACTORY OBJECT METHOD TO CREATE A BOAT OBJECT
-	this.engine = options.engine || "honda";
-	this.state = options.state || "brand new";
-	this.color = options.color || "white";
-}
-
-function Bike(options) { //FACTORY OBJECT METHOD TO CREATE A BIKE OBJECT
-	this.gears = options.gears || 12;
-	this.state = options.state || "new";
-	this.color = options.color || "blue";
+function Create_div(options) { //FACTORY OBJECT METHOD TO CREATE A BOAT OBJECT
+	var div = document.createElement('div');
+	div.src = options.src;
+	div.id = options.id;
+	div.className = options.className;
+	div.textContent = options.textContent;
 }
 
 
-function VehicleFactory() {} //CREATING THE EMPTY CLASS VEHICLEFACTORY
-VehicleFactory.prototype.createVehicle = function(options) { //PROTOTYPING THE CREATEVEHICLE METHOD OF THE VEHICLEFACTORY CLASS TO A FUNCTION THAT RETURN A NEW OBJECT
-	switch(options.vehicleType) {
-	case 'car':
-		return new Car(options);
-	case 'truck':
-		return new Truck(options);
-	case 'boat':
-		return new Boat(options);
-	case 'bike':
-		return new Bike(options);
+function ElementFactory() {} //CREATING THE EMPTY CLASS ElementFactory
+ElementFactory.prototype.createElements = function(options) { //PROTOTYPING THE createElements METHOD OF THE ElementFactory CLASS TO A FUNCTION THAT RETURN A NEW OBJECT
+	switch(options.elementType) {
+	case 'a':
+		return new Create_a(options);
+	case 'img':
+		return new Create_img(options);
+	case 'div':
+		return new Create_div(options);
 	default:
-		console.log(options.vehicleType + " is not a valid vehicle type.");
+		console.log(options.vehicleType + " is not a valid element");
 	}
 
 };
@@ -47,11 +45,11 @@ VehicleFactory.prototype.createVehicle = function(options) { //PROTOTYPING THE C
 
 //SINGLETON IMPLEMENTATION
 var instance; //GLOBAL VARIABLE USED TO TRACK INSTANCE
-var VehicleSingleton = function() {
+var ElementFactorySingleton = function() {
 
 
 		function init() { //FUNCTION USED TO INITIALIZE AN INSTANCE OF THE VEHICLE FACTORY
-			return new VehicleFactory();
+			return new ElementFactory();
 
 		}
 
@@ -65,47 +63,51 @@ var VehicleSingleton = function() {
 
 //USAGE
 
-var outputFactory = VehicleSingleton(); //CREATING A NEW VEHICLEFACTORY OBJECT - USED FOR VALIDATION OF SINGLETON
-var outputFactory2 = VehicleSingleton(); //USED FOR VALIDATION OF SINGLETON
-var car = outputFactory.createVehicle({
-	vehicleType: "car",
-	color: "yellow",
-	doors: 6
+var outputFactory = ElementFactorySingleton(); //CREATING A NEW ElementFactory OBJECT - USED FOR VALIDATION OF SINGLETON
+var outputFactory2 = ElementFactorySingleton(); //USED FOR VALIDATION OF SINGLETON
+var a = outputFactory.createElements({
+	elementType: "a",
+	href: "http://www.google.com",
+	id: "goole_link",
+	className: "links"
 });
 
-var boat = outputFactory.createVehicle({
-	vehicleType: "boat",
-	engine: "kawasaki",
-	state: "used"
+var div = outputFactory.createElements({
+	elementType: "div",
+	id: "container",
+	className: "clearfix",
+	textContent: "This is the world as we know it"
 });
 
-var bike = outputFactory.createVehicle({
-	vehicleType: "bike",
-	gears: 10,
-	color: "purple"
-	
-
-});
-
-var pinkBike = outputFactory.createVehicle({
-	vehicleType: "bIke",
-	gears: 13,
-	color: "pink"
-	
+var img = outputFactory.createElements({
+	elementType: "img",
+	id: "nick_cage",
+	className: "pics",
+	textContent: "The One True God",
+	src: "http://ia.media-imdb.com/images/M/MV5BMTUzMDM4Nzk2MV5BMl5BanBnXkFtZTcwNTcwNjExOQ@@._V1._SY314_CR0,0,214,314_.jpg"
 
 });
 
-console.log(car instanceof Car); //TRUE
+var img_misspelled = outputFactory.createElements({
+	elementType: "imd", // Misspleled 
+	id: "nick_cage",
+	className: "pics",
+	textContent: "The One True God",
+	src: "http://ia.media-imdb.com/images/M/MV5BMTUzMDM4Nzk2MV5BMl5BanBnXkFtZTcwNTcwNjExOQ@@._V1._SY314_CR0,0,214,314_.jpg"
 
-console.log(boat instanceof Boat); //TRUE
+});
 
-console.log(bike instanceof Bike); //TRUE
+console.log(a instanceof Create_a); //TRUE
 
-console.log(pinkBike instanceof Bike); //FALSE - wrong vehicle type
+console.log(div instanceof Create_div); //TRUE
+
+console.log(img instanceof Create_img); //TRUE
+
+console.log(img_misspelled instanceof Create_img); //FALSE - wrong vehicle type
 
 console.log((outputFactory === outputFactory2) + " -- SINGLETON success"); //TRUE - The two variables point to the same object in memory.
 
-console.log(car);
-console.log(boat);
-console.log(bike);
-console.log(pinkBike);
+console.log(a);
+console.log(div);
+console.log(img);
+console.log(img_misspelled);
